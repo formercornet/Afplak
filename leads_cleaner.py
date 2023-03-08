eafb9205168678fc907c3b1f99d0dca02bb64829
Hussain <senobadrello@gmail.com>
2023-03-08 22:26:23 +0200

    Perfect na2es duplicates and u003e

--- leads_cleaner.py
+++ leads_cleaner.py
@@ -1,11 +1,27 @@
 import csv
 import os
 
-blacklist = set()
+# Read the banned substrings from a set file
+banned_substrings = set()
+with open('banned_substrings.txt', 'r') as f:
+    for line in f:
+        banned_substrings.add(line.strip())
 
+# Read the email blacklist from a file
+blacklist = set()
 with open('blacklist.txt', 'r') as f:
     for line in f:
         blacklist.add(line.strip())
+
+# Define a function to remove duplicates from a list of emails
+def clean_emails(emails):
+    cleaned_emails = []
+    seen_emails = set()
+    for email in emails:
+        if email not in seen_emails and not any(banned_substring in email for banned_substring in banned_substrings) and email not in blacklist:
+            cleaned_emails.append(email)
+            seen_emails.add(email)
+    return cleaned_emails
 
 with open('leads.csv', 'r') as f, open('output.csv', 'w', newline='') as out_f:
     reader = csv.reader(f)
@@ -14,10 +30,7 @@ with open('leads.csv', 'r') as f, open('output.csv', 'w', newline='') as out_f:
     writer.writerow(header) # write header to output file
     for row in reader:
         emails = row[2].split(",")
-        cleaned_emails = []
-        for email in emails:
-            if not any(banned_word in email for banned_word in blacklist):
-                cleaned_emails.append(email)
+        cleaned_emails = clean_emails(emails)
         row[2] = ",".join(cleaned_emails)
         writer.writerow(row)
 
